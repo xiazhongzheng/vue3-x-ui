7930c60cfeab07316818a0a8334e2a753381658e
xiazhongzheng <1107041574@qq.com>
2021-04-13 23:45:07 +0800

    row-col

--- packages/x-ui/index.ts
+++ packages/x-ui/index.ts
@@ -2,11 +2,15 @@ import { App } from 'vue';
 import Button from '@x-ui/button';
 import Icon from '@x-ui/icon';
 import ButtonGroup from '@x-ui/button-group';
+import Row from '@x-ui/row';
+import Col from '@x-ui/col';
 
 const components = [
     Button,
     Icon,
-    ButtonGroup
+    ButtonGroup,
+    Row,
+    Col
 ]
 
 const install = (app: App): void => {
--- website/App.vue
+++ website/App.vue
@@ -30,6 +30,23 @@
         >下一页<i class="x-icon-arrow-right x-icon--right"></i
       ></x-button>
     </x-button-group>
+
+    <x-row>
+      <x-col :span="6" style="background-color: red">111</x-col>
+      <x-col :span="6" :offset="6" style="background-color: green">222</x-col>
+      <x-col :span="6" style="background-color: blue">333</x-col>
+    </x-row>
+
+    <x-row :gutter="20">
+      <x-col :span="8" style="background-color: red">111</x-col>
+      <x-col :span="8" style="background-color: green">222</x-col>
+      <x-col :span="8" style="background-color: blue">333</x-col>
+    </x-row>
+    <x-row :gutter="20" :justify="'around'">
+      <x-col :span="2" style="background-color: red">111</x-col>
+      <x-col :span="2" style="background-color: green">222</x-col>
+      <x-col :span="2" style="background-color: blue">333</x-col>
+    </x-row>
   </div>
 </template>
 
